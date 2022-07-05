34c99697850ae747d6a0b4b03df17da0b30a7555
Kevin Capoccetti <75384575+colorwebfr@users.noreply.github.com>
2022-07-05 14:19:43 +0100

    Create content-single-product.php

---
+++ content-single-product.php
@@ -0,0 +1,8 @@
+//Get image cat for this product
+			$terms = get_the_terms( $post->ID, 'product_cat' );
+			foreach ( $terms as $term )
+			{
+				$category_thumbnail = get_woocommerce_term_meta($term->term_id, 'thumbnail_id', true);
+				$image = wp_get_attachment_url($category_thumbnail);
+			}
+			echo '<img src="'.$image.'" alt="Logo '.$term->name.'">';
